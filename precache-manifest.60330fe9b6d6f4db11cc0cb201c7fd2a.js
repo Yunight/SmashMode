bafbef4cbf6e5ec7a76a16d79daae806162eb706
benchi chen <benchi>
2020-06-24 22:33:41 +0200

    Updates

---
+++ precache-manifest.60330fe9b6d6f4db11cc0cb201c7fd2a.js
@@ -0,0 +1,330 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8fff8e883439e3242729f2ef6ffbdf",
+    "url": "/SmashMode/index.html"
+  },
+  {
+    "revision": "9101c7eeb32a726b1af4",
+    "url": "/SmashMode/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eeebf0a63db66febcacb",
+    "url": "/SmashMode/static/js/2.a5edeb7c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SmashMode/static/js/2.a5edeb7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9101c7eeb32a726b1af4",
+    "url": "/SmashMode/static/js/main.0eb439e5.chunk.js"
+  },
+  {
+    "revision": "96bf559006c281cd2d92",
+    "url": "/SmashMode/static/js/runtime-main.2a39a903.js"
+  },
+  {
+    "revision": "68056c5efc7a256d99a3ed8891bc5778",
+    "url": "/SmashMode/static/media/01_Mario_SSBU.68056c5e.png"
+  },
+  {
+    "revision": "c32a70a095fd7e72c8e3f8f04c6df4cf",
+    "url": "/SmashMode/static/media/02_Donkey_Kong_SSBU.c32a70a0.png"
+  },
+  {
+    "revision": "8298f4717e610529c85b0863637413fa",
+    "url": "/SmashMode/static/media/03_Link_SSBU.8298f471.png"
+  },
+  {
+    "revision": "b30502c67c90a7b6cab188c061f130d8",
+    "url": "/SmashMode/static/media/04_Samus_SSBU.b30502c6.png"
+  },
+  {
+    "revision": "ff2e4184aec0f53e59ba692aa4e0468b",
+    "url": "/SmashMode/static/media/04e_Dark_Samus_SSBU.ff2e4184.png"
+  },
+  {
+    "revision": "ceb9beab83999be8f82fba2231dfec4d",
+    "url": "/SmashMode/static/media/05_Yoshi_SSBU.ceb9beab.png"
+  },
+  {
+    "revision": "ff47969f9fa5c461e421f73db4d4dd3f",
+    "url": "/SmashMode/static/media/07_Fox_SSBU.ff47969f.png"
+  },
+  {
+    "revision": "0cd3aacfc56bb7edeabd97c796d5e7a8",
+    "url": "/SmashMode/static/media/08_Pikachu_SSBU.0cd3aacf.png"
+  },
+  {
+    "revision": "ea5787f5e68d13a9eed3e7b5a5d70bc4",
+    "url": "/SmashMode/static/media/09_Luigi_SSBU.ea5787f5.png"
+  },
+  {
+    "revision": "a5d893693f7fd4b087020de832e20fdd",
+    "url": "/SmashMode/static/media/10_Ness_SSBU.a5d89369.png"
+  },
+  {
+    "revision": "a937634a8c90d228f820a4ec0550db58",
+    "url": "/SmashMode/static/media/11_CaptainFalcon_SSBU.a937634a.png"
+  },
+  {
+    "revision": "330bbce10e1305524994fd2bffcc8058",
+    "url": "/SmashMode/static/media/12_Jigglypuff_SSBU.330bbce1.png"
+  },
+  {
+    "revision": "60d4ba37b520c5d2e4805df14486af24",
+    "url": "/SmashMode/static/media/13_Peach_SSBU.60d4ba37.png"
+  },
+  {
+    "revision": "7f5a7c15fd0b79bffd745193a034a985",
+    "url": "/SmashMode/static/media/13e_Daisy_SSBU.7f5a7c15.png"
+  },
+  {
+    "revision": "ed73e91f39c9beaec1baf1501ae2a706",
+    "url": "/SmashMode/static/media/14_Bowser_SSBU.ed73e91f.png"
+  },
+  {
+    "revision": "af289c18ca0194844d4123ff21f10e7b",
+    "url": "/SmashMode/static/media/15_IceClimbers_SSBU.af289c18.png"
+  },
+  {
+    "revision": "5c4c1aa16795e9330f262209043ac20e",
+    "url": "/SmashMode/static/media/16_Sheik_SSBU.5c4c1aa1.png"
+  },
+  {
+    "revision": "e01fa463f9dbae42b4df202f4bc69bb2",
+    "url": "/SmashMode/static/media/17_Zelda_SSBU.e01fa463.png"
+  },
+  {
+    "revision": "70d85de7aa43c6ea960d207ac605f8b2",
+    "url": "/SmashMode/static/media/18_DrMario_SSBU.70d85de7.png"
+  },
+  {
+    "revision": "f3d6ffadabc2aaee118ca985f1e006c9",
+    "url": "/SmashMode/static/media/19_Pichu_SSBU.f3d6ffad.png"
+  },
+  {
+    "revision": "5fd02f3ef93a82e13e64068e37f45d18",
+    "url": "/SmashMode/static/media/20_Falco_SSBU.5fd02f3e.png"
+  },
+  {
+    "revision": "8ef597b37063707f116c0197844c31df",
+    "url": "/SmashMode/static/media/21_Marth_SSBU.8ef597b3.png"
+  },
+  {
+    "revision": "def658afb6308a206238e59170a3ad0d",
+    "url": "/SmashMode/static/media/21e_Lucina_SSBU.def658af.png"
+  },
+  {
+    "revision": "f8c3ebb545d698f58cb4d0312c6cfd11",
+    "url": "/SmashMode/static/media/22_YoungLink_SSBU.f8c3ebb5.png"
+  },
+  {
+    "revision": "188fdd9325d6461250400880a4ccc1c4",
+    "url": "/SmashMode/static/media/23_Ganondorf_SSBU.188fdd93.png"
+  },
+  {
+    "revision": "0f521cc7a2b03199b5abeee360225ecf",
+    "url": "/SmashMode/static/media/24_Mewtwo_SSBU.0f521cc7.png"
+  },
+  {
+    "revision": "966dbfbdf67290e981c661dfcb2d5320",
+    "url": "/SmashMode/static/media/25_Roy_SSBU.966dbfbd.png"
+  },
+  {
+    "revision": "5347790a8c388aaf40d5a1a4ccb80be9",
+    "url": "/SmashMode/static/media/25e_Fox_SSBU.5347790a.png"
+  },
+  {
+    "revision": "a35ed269d5bf96499f6c198ed2f3304a",
+    "url": "/SmashMode/static/media/27_Meta_Knight_SSBU.a35ed269.png"
+  },
+  {
+    "revision": "e8d3d71882cb3b609e77b9117fa4b16c",
+    "url": "/SmashMode/static/media/28_Pit_SSBU.e8d3d718.png"
+  },
+  {
+    "revision": "c033f85c7c9abc4b206308a39de5c26b",
+    "url": "/SmashMode/static/media/28e_Pit_SSBU.c033f85c.png"
+  },
+  {
+    "revision": "1672b243a71a12f6974b2bf4f3335e8c",
+    "url": "/SmashMode/static/media/29_Zero_Suit_Samus_SSBU.1672b243.png"
+  },
+  {
+    "revision": "6ee8dfe46192e84ee85cae58775a6996",
+    "url": "/SmashMode/static/media/30_Wario_SSBU.6ee8dfe4.png"
+  },
+  {
+    "revision": "5e73c7b16a5503f14766945fb13c4fd1",
+    "url": "/SmashMode/static/media/31_Snake_SSBU.5e73c7b1.png"
+  },
+  {
+    "revision": "1a44fa87e2386f448129c2cba9ca28d5",
+    "url": "/SmashMode/static/media/32_Ike_SSBU.1a44fa87.png"
+  },
+  {
+    "revision": "f1c131651c7e478bc8ac058384d045eb",
+    "url": "/SmashMode/static/media/33_34_35_Pokémon_Trainer_(solo)_SSBU.f1c13165.png"
+  },
+  {
+    "revision": "90c738ca9873bb3c5965d082bf434951",
+    "url": "/SmashMode/static/media/36_DiddyKong_SSBU.90c738ca.png"
+  },
+  {
+    "revision": "deb85de61db0f5250d900cc6303519b6",
+    "url": "/SmashMode/static/media/37_Lucas_SSBU.deb85de6.png"
+  },
+  {
+    "revision": "b01ab3f8533af34f1691ead46e98c180",
+    "url": "/SmashMode/static/media/38_Sonic_SSBU.b01ab3f8.png"
+  },
+  {
+    "revision": "11a9f08e5534e2c5b5eaf5fc35c54ff2",
+    "url": "/SmashMode/static/media/39_King_Dedede_SSBU.11a9f08e.png"
+  },
+  {
+    "revision": "a52d2a610ea2f7887df51e9600f4be27",
+    "url": "/SmashMode/static/media/40_Olimar_SSBU.a52d2a61.png"
+  },
+  {
+    "revision": "8308ad71f984e728f5077f6dd873b60f",
+    "url": "/SmashMode/static/media/41_Lucario_SSBU.8308ad71.png"
+  },
+  {
+    "revision": "a971531c7609e4980ffec25916186420",
+    "url": "/SmashMode/static/media/42_R.O.B._SSBU.a971531c.png"
+  },
+  {
+    "revision": "6a8207a987bf5ea27deb95afc070b8c5",
+    "url": "/SmashMode/static/media/43_ToonLink_SSBU.6a8207a9.png"
+  },
+  {
+    "revision": "6d8e38415b0596f89f1f617b64d6213d",
+    "url": "/SmashMode/static/media/44_Wolf_SSBU.6d8e3841.png"
+  },
+  {
+    "revision": "0a68db6916a070e01060d64b52ac3008",
+    "url": "/SmashMode/static/media/45_Villager_SSBU.0a68db69.png"
+  },
+  {
+    "revision": "461f357b0922de929b11adf4420e88a0",
+    "url": "/SmashMode/static/media/46_Mega_Man_SSBU.461f357b.png"
+  },
+  {
+    "revision": "90f3f2f8c2321a50f55aa317ca3cbe2c",
+    "url": "/SmashMode/static/media/48_Rosalina_&_Luma_SSBU.90f3f2f8.png"
+  },
+  {
+    "revision": "cbefb630ddb6dd0b540701c7212187f2",
+    "url": "/SmashMode/static/media/49_Little_Mac_SSBU.cbefb630.png"
+  },
+  {
+    "revision": "c93751b68fc18c81a1b43b58e5e897b4",
+    "url": "/SmashMode/static/media/50_Greninja_SSBU.c93751b6.png"
+  },
+  {
+    "revision": "f505847c55b6a1efecaacc94fe3b2414",
+    "url": "/SmashMode/static/media/51_Mii_Brawler_SSBU.f505847c.png"
+  },
+  {
+    "revision": "afd904e258010dff2bcc373cc6c83b5b",
+    "url": "/SmashMode/static/media/54_Palutena_SSBU.afd904e2.png"
+  },
+  {
+    "revision": "01a8ea14ed4635b7cbd396995ba45728",
+    "url": "/SmashMode/static/media/55_Pac-Man_SSBU.01a8ea14.png"
+  },
+  {
+    "revision": "aef8dda5aab55fa0aa691ee41c8e1b0d",
+    "url": "/SmashMode/static/media/56_Robin_SSBU.aef8dda5.png"
+  },
+  {
+    "revision": "f1105426a6c6ae55c3356947e0090e7e",
+    "url": "/SmashMode/static/media/57_Shulk_SSBU.f1105426.png"
+  },
+  {
+    "revision": "7a3d62205e1d7ff51bca5c8d48df5cc8",
+    "url": "/SmashMode/static/media/58_Bowser_Jr._SSBU.7a3d6220.png"
+  },
+  {
+    "revision": "a3b3bc1b6418c2238bebf8cd3b92027f",
+    "url": "/SmashMode/static/media/59_Duck_Hunt_SSBU.a3b3bc1b.png"
+  },
+  {
+    "revision": "194c7e433e344fa186b0f600699eff1e",
+    "url": "/SmashMode/static/media/60_Ryu_SSBU.194c7e43.png"
+  },
+  {
+    "revision": "56f15f80e95a057f6b50f65213b35a60",
+    "url": "/SmashMode/static/media/60e_Ken_SSBU.56f15f80.png"
+  },
+  {
+    "revision": "758d6944ee72c8dafba42b6c3c63a95a",
+    "url": "/SmashMode/static/media/61_Cloud_SSBU.758d6944.png"
+  },
+  {
+    "revision": "8b68663c989aeb76e1cf36e48ff75842",
+    "url": "/SmashMode/static/media/62_Corrin_SSBU.8b68663c.png"
+  },
+  {
+    "revision": "72de124f29c07da6d56140b44f0febed",
+    "url": "/SmashMode/static/media/63_Bayonetta_SSBU.72de124f.png"
+  },
+  {
+    "revision": "27b3824359569045c75dc45f4aacb633",
+    "url": "/SmashMode/static/media/64_Inkling_SSBU.27b38243.png"
+  },
+  {
+    "revision": "28d8326b12a25f7adb9c46e2c21183b1",
+    "url": "/SmashMode/static/media/65_Ridley_SSBU.28d8326b.png"
+  },
+  {
+    "revision": "3d267d0c69724efdd61a96e554f355eb",
+    "url": "/SmashMode/static/media/66_Richter_SSBU.3d267d0c.png"
+  },
+  {
+    "revision": "2155e2e56e12f653a520bf5e81693f10",
+    "url": "/SmashMode/static/media/66_Simon_SSBU.2155e2e5.png"
+  },
+  {
+    "revision": "9e541e42563f0110b4705658839cfa07",
+    "url": "/SmashMode/static/media/67_King_K._Rool_SSBU.9e541e42.png"
+  },
+  {
+    "revision": "bd50a49cdd862aff202e5dec7df42db2",
+    "url": "/SmashMode/static/media/68_Isabelle_SSBU.bd50a49c.png"
+  },
+  {
+    "revision": "d16fa66f479ca5b77ee0c4991351c1dd",
+    "url": "/SmashMode/static/media/69_Incineroar_SSBU.d16fa66f.png"
+  },
+  {
+    "revision": "6c086298b43a93c0ce6e52ae47cca556",
+    "url": "/SmashMode/static/media/70_Piranha_Plant_SSBU.6c086298.png"
+  },
+  {
+    "revision": "70123984115aa377dc0078f1bdef2d52",
+    "url": "/SmashMode/static/media/71_Joker_SSBU.70123984.png"
+  },
+  {
+    "revision": "134d6278c186e5afaa20194bca66d269",
+    "url": "/SmashMode/static/media/72_Hero_SSBU.134d6278.png"
+  },
+  {
+    "revision": "9fa20f64f28cf8b61e32aa28d3dc8f09",
+    "url": "/SmashMode/static/media/73_Banjo_&_Kazooie_SSBU.9fa20f64.png"
+  },
+  {
+    "revision": "1b2cc4834228bfb12c05ab14e99c5eb3",
+    "url": "/SmashMode/static/media/74_Terry_SSBU.1b2cc483.png"
+  },
+  {
+    "revision": "610298fe119e5838e79f350226b79358",
+    "url": "/SmashMode/static/media/75_Byleth_SSBU.610298fe.png"
+  },
+  {
+    "revision": "ca0408d2168193d1a248692aefcdc4b9",
+    "url": "/SmashMode/static/media/Min_Min_SSBU.ca0408d2.png"
+  }
+]);
