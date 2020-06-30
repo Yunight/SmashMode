7a67a9d504a327eaba2ae294a493c5d48f8c1077
benchi chen <benchi>
2020-06-30 02:03:37 +0200

    GA added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React,{useEffect} from "react";
 import FightersBuild from "./Components/FightersBuild";
+import ReactGA from "react-ga";  //Google Analytics
 
 export default function SmashNOL() {
+
+    useEffect(() => {
+        ReactGA.initialize('UA-171288298-1');
+        ReactGA.pageview(window.location.pathname);
+    });
+
 
     return (
         <FightersBuild/>
